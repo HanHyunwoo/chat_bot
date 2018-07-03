f21c63aaf66781f74e08d4046d210d3a1e629ec6
ChangHee <och8808@naver.com>
2018-07-03 04:48:12 +0000

    cat

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -16,21 +16,45 @@ class KakaoController < ApplicationController
       @text = ["20층","멀캠식당","급식"].sample
     elsif @user_msg == "로또"
       @text = (1..45).to_a.sample(6).sort.to_s
+    elsif @user_msg == "고양이"
+      @url = "http://thecatapi.com/api/images/get?format=xml&type=jpg"
+      @cat_xml = RestClient.get(@url)
+      @cat_doc = Nokogiri::XML(@cat_xml)
+      @cat_url = @cat_doc.xpath("//url").text
+      @text = @cat_url
     end
     
     
     @return_msg ={
       :text => @text
     }
+    @return_msg_photo = {
+      :text => "나만 고양이 없어 :(",
+      :photo => {
+        :url => @cat_url,
+        :width => 720,
+        :height => 630
+        
+      }
+    }
+    
     @return_keyboard = {
         :type => "buttons",
         :buttons => ["메뉴", "로또", "고양이"]
     }
     
-    @result = {
-      :message => @return_msg,
-      :keyboard => @return_keyboard
-    }
+    if @user_msg == "고양이"
+      @result = {
+        :message => @return_msg_photo,
+        :keyboard => @return_keyboard
+      }
+    else
+      @result = {
+        :message => @return_msg,
+        :keyboard => @return_keyboard
+      }
+    end
+    
     render json: @result
   end
 end
